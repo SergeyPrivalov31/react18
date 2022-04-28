fc312399a7ed0b6386338d30977b088774ad8570
Privalov Sergei <SPrivalov@SITRONICS.COM>
2022-04-28 14:46:11 +0300

    fix

---
+++ src/newHooks/text.jsx
@@ -0,0 +1,10 @@
+//useId() -> даёт уникальное значение id
+//
+//только в библиотеках ->   useSyncExternalStore(), useInsertionEffect(didUpdate),
+
+//SSR добавили Lazy и Suspense
+
+//Offscreen  -> для того чтобы компонент сохранял своё состояние при удалении \ появлении
+
+//ServerComponents ??? -> для создателей библиотек
+
