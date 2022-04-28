59d3f9b5f92a17b70da40c6c2b79e08f52aff998
Privalov Sergei <SPrivalov@SITRONICS.COM>
2022-04-28 12:11:53 +0300

    react-18 Batching

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Batching} from './examples/Batching';
+
+const App = () => {
+  return (
+    <div>
+      <Batching />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/examples/Batching.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { flushSync } from "react-dom";
+
+//ЭТА ШТУКА ПОЗВОЛЯЕТ ОПТИМИЗИРОВАТЬ КОД, ОПТИМИЗИРОВАТЬ ПЕРЕРИСОВКИ
+
+export const Batching = () => {
+
+    const [value, setValue] = useState(0)
+    const [state, setState] = useState(0)
+
+    console.log('RENDER')
+
+    ////Тут Batching работает, один рендер происходит
+
+    const update = () => {
+        Promise.resolve()
+            .then(() => {
+                setValue(prev => prev + 1)  
+                setState(prev => prev + 1)
+            })
+        }      
+    
+    //// Тут Batching не работает, два рендера происходит
+    
+    // const update = () => {
+    //     Promise.resolve()
+    //         .then(() => {
+    //             flushSync(() => { setValue(prev => prev + 1) }) 
+    //             flushSync(() => { setState(prev => prev + 1) })
+    //         })
+    //     }
+
+    return (
+            <div>
+                <h1>value {value}</h1>
+                <h1>state {state}</h1>
+                <button onClick={update}>UPDATE</button>
+            </div>
+        )
+}
